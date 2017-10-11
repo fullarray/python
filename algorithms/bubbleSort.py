bec70cbfea969c9a0c622c235b0a5497a4a57a87
Jonathan <sifreed77@gmail.com>
2017-10-10 22:26:17 -0400

    Added bubble sort implementation

---
+++ algorithms/bubbleSort.py
@@ -0,0 +1,16 @@
+def bubbleSorting(myNumList):
+    moreSwaps = True
+    while moreSwaps:
+        moreSwaps = False
+        for element in range(len(myNumList)-1):
+            if myNumList[element] > myNumList[element+1]:
+                moreSwaps = True
+                temp = myNumList[element]
+                myNumList[element] = myNumList[element+1]
+                myNumList[element+1]=temp
+    return myNumList
+
+def bubbleSortingTest():
+    myNumList = [4,5,3,9,29,3,4,65]
+    sortedList = bubbleSorting(myNumList)
+    print(sortedList)
