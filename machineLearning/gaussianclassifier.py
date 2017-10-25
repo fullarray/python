d8cefe8126904a2e3f6be24ff4ceb72e743e7653
Jonathan <sifreed77@gmail.com>
2017-10-24 22:58:47 -0400

    Added Gaussian Bayes classifier implementation

---
+++ machineLearning/gaussianclassifier.py
@@ -0,0 +1,19 @@
+#import library gaussian naive bayes
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+
+#target variables
+x= np.array([[-2,7],[1,5], [1,4], [-2,8], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-6,9]])
+y = np.array([8, 3, 3, 3, 4, 3, 3, 4, 1, 4, 4, 1])
+
+#Create a Gaussian Classifier
+model = GaussianNB()
+
+#model training
+model.fit(x,y)
+
+#predictor
+predicting=model.predict([[1,2],[3,8]])
+
+#print to screen
+print predicting
