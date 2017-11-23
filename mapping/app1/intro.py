44bccfa014e7ff3db5b1a7c5f16b3d3f16b9c06e
Jonathan <sifreed77@gmail.com>
2017-11-22 23:26:16 -0500

    Added pcap file read implementation

---
+++ mapping/app1/intro.py
@@ -0,0 +1,9 @@
+from scapy.all import *
+
+# rdpcap comes from scapy and loads in our pcap file
+packets = rdpcap('smtp.pcap')
+
+# Let's iterate through every packet
+#for packet in packets:
+#    print(packet)
+packets.summary()
