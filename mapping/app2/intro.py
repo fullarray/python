9899be146a094f8b6a3023235c769a228538b36b
Jonathan <sifreed77@gmail.com>
2017-11-23 23:36:22 -0500

    Added 2nd pcap reading implementation

---
+++ mapping/app2/intro.py
@@ -0,0 +1,25 @@
+
+import scapy
+from scapy.all import PcapReader
+
+# rdpcap comes from scapy and loads in our pcap file
+#packets = rdpcap('smtp.pcap')
+
+# Let's iterate through every packet
+#for packet in packets:
+#    print(packet)
+#packets.summary()
+
+with PcapReader('smtp.pcap') as pcap_reader:
+ for pkt in pcap_reader:
+     print (pkt)
+
+# import pcapng
+# from pcapng import FileScanner
+# import sys
+# 
+# with open('scannet.pcap', 'rb') as fp:
+#     scanner = pcapng.FileScanner(fp)
+#     blocks = list(scanner)
+#     for block in blocks:
+#          print(block)
