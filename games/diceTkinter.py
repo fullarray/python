105dba1cc3abf54367634ed74af14ada1dd50192
Jonathan <sifreed77@gmail.com>
2017-12-24 22:34:27 -0500

    Added dice game implementation in tkinter

---
+++ games/diceTkinter.py
@@ -0,0 +1,19 @@
+import tkinter
+import random
+
+class DiceFrm(tkinter.Frame):
+	def __init__(self, master):
+		super().__init__(master)
+		
+		die = tkinter.Button(self, text="Roll", command=self.roll)
+		die.pack()
+		self.roll_result = tkinter.StringVar()
+		label = tkinter.Label(self, textvariables=self.roll_result)
+		label.pack()
+		self.pack()
+	
+	def roll(self):
+		self.roll_result.set(random.randint(1, 6))
+	
+root = tkinter.Tk()
+DiceFrm(master=root).mainloop()
