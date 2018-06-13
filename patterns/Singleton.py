0786f075a5c188d0d8f34c4bf470b6e1c5e1a243
Jonathan <sifreed77@gmail.com>
2018-06-12 23:37:14 -0400

    Added singleton implementation

---
+++ patterns/Singleton.py
@@ -0,0 +1,20 @@
+class Singleton(type)
+	def __init__(cls, name, bases, attrs, **kwargs):
+		super().__init__(name, bases, attrs)
+		cls._instance = None
+	
+	def __call__(cls, *args, **kwargs):
+		if cls._instance is None:
+			cls._instance = super().__call__(*args, **kwargs)
+		return cls._instance
+
+class SampleClass(metaclass=Singleton):
+	pass
+
+def main():
+	o1 = SampleClass()
+	o2 = SampleClass()
+	assert o1 is o2
+	
+if __name__ == "__main__":
+	main()
