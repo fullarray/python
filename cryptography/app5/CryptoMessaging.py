23a681294cd74f720cfc579cdc208bab236dcf8a
Jonathan <sifreed77@gmail.com>
2018-09-24 23:26:47 -0400

    Update CryptoMessaging.py

--- cryptography/app5/CryptoMessaging.py
+++ cryptography/app5/CryptoMessaging.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\swb\Documents\Projects\py\Crypto Messaging.ui'
+# Form implementation generated from reading ui file 'C:\Users\xxxxx\Documents\Projects\py\Crypto Messaging.ui'
 #
 # Created by: PyQt5 UI code generator 5.10.1
 #
