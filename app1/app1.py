dcbb2b272ec018734060415201fe9b7fc2eb53fd
Jonathan <sifreed77@gmail.com>
2019-06-28 23:02:40 -0400

    Update app1.py

--- app1/app1.py
+++ app1/app1.py
@@ -1,5 +1,5 @@
 import nltk
-"nltk.download()" #this line is questionable
+"nltk.download()" #this line is questionables
 from nltk.corpus import wordnet as wn
 panda = wn.synset('panda.n.01')
 hyper = lambda s: s.hypernyms()
