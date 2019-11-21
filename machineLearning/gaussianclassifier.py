e9535e0cfea2f8f9b82201304d8b239781684b70
Jonathan <sifreed77@gmail.com>
2019-11-20 23:38:47 -0500

    Update gaussianclassifier.py

--- machineLearning/gaussianclassifier.py
+++ machineLearning/gaussianclassifier.py
@@ -6,7 +6,7 @@ import numpy as np
 x= np.array([[-2,7],[1,5], [1,4], [-2,8], [2,3], [-4,1], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-6,9]])
 y = np.array([8, 3, 3, 3, 4, 3, 3, 4, 1, 4, 4, 1])
 
-#Create a Gaussian Classifier
+#Create a Gaussian Classifiers
 model = GaussianNB()
 
 #model training
