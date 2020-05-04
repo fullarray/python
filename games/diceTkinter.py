a749915ea81b255fdb6d0dd2cf22aa12f85a63ab
Jonathan <sifreed77@gmail.com>
2020-05-03 22:44:41 -0400

    Update diceTkinter.py

--- games/diceTkinter.py
+++ games/diceTkinter.py
@@ -3,7 +3,7 @@ import random
 class DiceFrm(tkinter.Frame):
 	def __init__(self, master):
 		super().__init__(master)
-		die = tkinter.Button(self, text="Roll Dice", command=self.roll)
+		die = tkinter.Button(self, text="Roll Dices", command=self.roll)
 		die.pack()
 		self.roll_result = tkinter.StringVar()
 		label = tkinter.Label(self, textvariables=self.roll_result)
@@ -12,6 +12,5 @@ class DiceFrm(tkinter.Frame):
 	def roll(self):
 		self.roll_result.set(random.randint(1, 6))
 
-		
 root = tkinter.Tk()
 DiceFrm(master=root).mainloop()
