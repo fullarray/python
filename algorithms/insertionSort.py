1b7f84e094aa88101b5211a974f08ab9f8167565
Jonathan <sifreed77@gmail.com>
2020-06-22 23:31:45 -0400

    Update insertionSort.py

--- algorithms/insertionSort.py
+++ algorithms/insertionSort.py
@@ -2,6 +2,7 @@ def insertionSort(alist):
     for index in range(1, len(alist)):
         currentValue = alist[index]
         position = index
+        
         while position>0 and alist[position-1]>currentValue:
             alist[position]=alist[position-1]
             position=position-1
