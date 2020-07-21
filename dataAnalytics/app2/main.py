5c013fb68cf9813bcc05134ef4e954e66128da25
Jonathan <sifreed77@gmail.com>
2020-07-20 23:37:14 -0400

    Update main.py

--- dataAnalytics/app2/main.py
+++ dataAnalytics/app2/main.py
@@ -38,7 +38,7 @@ class LinearRegression(Object):
 		plt.show()
 		
 
-#Load the dataset and parse its
+#Load the dataset and parse it
 def load_dataset():
 	num_rows = sum(1 for line in open('Facebook_metrics\\dataset_Facebook.csv')) - 1
 	X = np.zeros((num_rows, 1))
