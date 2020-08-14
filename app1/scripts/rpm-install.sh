23e12cb0f71d6238f1848edd02f4106f4f754794
Jonathan <sifreed77@gmail.com>
2020-08-13 23:54:33 -0400

    Update rpm-install.sh

--- app1/scripts/rpm-install.sh
+++ app1/scripts/rpm-install.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
-#
 # This file becomes the install section of the generated spec file.
-#
 
 # This is what dist.py normally does.
 %{__python} setup.py install --root=${RPM_BUILD_ROOT} --record="INSTALLED_FILES"
