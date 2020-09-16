be94faa77690a5afa6ce5b243dc9ce6fdd977bc6
Jonathan <sifreed77@gmail.com>
2020-09-15 22:58:37 -0400

    Update hand_signs_deep_learning_analysis1.py

--- handCode/hand_signs_deep_learning_analysis1.py
+++ handCode/hand_signs_deep_learning_analysis1.py
@@ -3,9 +3,9 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-# import warnings
+# import warning
 import warnings
-# filter warnings
+# filter warning
 warnings.filterwarnings('ignore')
 from subprocess import check_output
 print(check_output(["ls", "../input"]).decode("utf8"))
