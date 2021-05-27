703ecad9f19b639823f0d2675736ad7fc03e7f24
Jonathan <sifreed77@gmail.com>
2021-05-27 19:36:11 -0400

    Update app.py

--- cryptography/app2/app.py
+++ cryptography/app2/app.py
@@ -1,9 +1,11 @@
 from tkinter import *
 def stopProg(e):
+    
     root.destroy()
     
 root=Tk()
 button1=Button(root,
+               
  text="click to close")
 button1.pack()
 button1.bind('<Button-1>',stopProg)
