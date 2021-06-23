42b95fee05022f2878200f3b2841b71e3b60d579
Jonathan <sifreed77@gmail.com>
2021-06-22 22:16:38 -0400

    Update GANsImagesMNST.py

--- handCode/GANsImagesMNST.py
+++ handCode/GANsImagesMNST.py
@@ -1,7 +1,10 @@
 def mnist_data():
     compose = transforms.Compose(
+        
         [transforms.ToTensor(),
+         
          transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+         
         ])
     out_dir = './dataset'
     return datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)
